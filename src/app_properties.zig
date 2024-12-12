81769737c2284467bdb632b35e30be256da72e95
Logickin-Lambda <logickinlambda@protonmail.com>
2024-12-12 14:18:07 +1100

    - Removed the custom theme The custom theme file never works and I don't find any documentation nor I can find any response if this function is properly run; besides, I have a color picker for the background which might break the theme, so I decided to trash it.

--- src/app_properties.zig
+++ src/app_properties.zig
@@ -3,7 +3,7 @@ const rl = @import("raylib");
 const gui = @import("raygui");
 
 pub const AppProperties = struct {
-    speed_value: f32 = 1,
+    speed_value: f32 = 0.25,
     cells_color: rl.Color = rl.Color.init(0, 0, 0, 255),
     cells_color_inverted: bool = true,
     background_color: rl.Color = rl.Color.init(0, 0, 0, 0),
